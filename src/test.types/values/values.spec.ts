6dfa98d42eef4dcb0acf50aa0c9bdf3e1e64e069
Mario Andrés Gómez Arroyo <mario.gomez@onelinkbpo.com>
2022-06-05 00:53:36 -0500

    Adding new tests

---
+++ src/test.types/values/values.spec.ts
@@ -0,0 +1,49 @@
+import { dummyArray, DummyClass, dummyClass, dummyNaN, dummyNull, dummyNumber, dummyQuote, dummyUndefined, dummyWord } from "./values";
+
+describe('Testing values', () => {
+
+    test('Test if equal', () => {
+        expect(dummyWord).toEqual('Webhelp');
+        // expect(dummyArray[1].month).toEqual('october');
+        // expect(dummyWord == 'Webhelp').toBeTruthy(); // Conditionals, as a "boolean" data type, can be validated this way as well.
+    });
+
+    test('Test if equal with .toBe', () => {
+        // Use .toBe to compare primitive values, not OOP objects because of the different instances.
+        expect(dummyNumber).toBe(57);
+    });
+
+    test('Test if matches even with another elements', () => {
+        // Check if some element or elements (string, object, etc) is in the array
+        expect(dummyArray).toEqual(expect.arrayContaining([31, true]));
+        // It fails if some of declared is in the expected
+        // expect(dummyArray).not.toEqual(expect.arrayContaining([99]));
+    });
+
+    test('Test with regex', () => {
+        // Use this, for example, if you want to know if there's a pattern or substring in a whole string.
+        expect(dummyQuote).toMatch(/KnowledgeTransfer/);
+    });
+
+    test('Test if NaN', () => {
+        expect(dummyNaN).toBeNaN();
+        // expect(dummyNull).toBeNaN(); // This is gonna crash because of different data types.
+        // expect(dummyNaN).not.toBeNaN();
+    });
+
+    test('Test if null', () => {
+        expect(dummyNull).toBeNull();
+        // expect(dummyNull).not.toBeNull();
+    });
+
+    test('Test if Undefined', () => {
+        expect(dummyUndefined).toBeUndefined();
+    });
+
+    test('Test if is an instance of a class', () => {
+        expect(dummyClass).toBeInstanceOf(DummyClass);
+        // expect(typeof dummyNumber).toBe("number"); // Another way
+    });
+
+
+});
---
+++ src/test.types/values/values.ts
@@ -0,0 +1,13 @@
+export const dummyWord = 'Webhelp';
+export const dummyNumber = 57;
+export const dummyQuote = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiuKnowledgeTransfersmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
+
+export const dummyArray: any = [31, {month: 'october'}, true, 'Enablement'];
+
+
+export const dummyNull = null;
+export const dummyUndefined = undefined;
+export const dummyNaN = NaN;
+
+export class DummyClass {};
+export const dummyClass = new DummyClass();
