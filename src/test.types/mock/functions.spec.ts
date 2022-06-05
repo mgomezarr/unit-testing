eb80e6192539abfc1ecee5d1049d8e1ba469320e
Mario Andrés Gómez Arroyo <mario.gomez@onelinkbpo.com>
2022-06-05 01:06:46 -0500

    Checkpoint

--- src/test.types/mock/functions.spec.ts
+++ src/test.types/mock/functions.spec.ts
@@ -1,14 +1,14 @@
-import { funcionQueEspera, verdadero } from '../index';
+// import { funcionQueEspera, verdadero } from '../index';
 
 
-describe('Mock testing', () => {
-    let webhelp: boolean = true;
-    it('dummy', async () => {
-        let result = await funcionQueEspera(1000);
-        expect(result).toBeTruthy();
-    }, 2000);
+// describe('Mock testing', () => {
+//     let webhelp: boolean = true;
+//     it('dummy', async () => {
+//         let result = await funcionQueEspera(1000);
+//         expect(result).toBeTruthy();
+//     }, 2000);
 
-    test('verdadero', () => { 
-        expect(true).toBe(true);
-    });
-});
+//     test('verdadero', () => { 
+//         expect(true).toBe(true);
+//     });
+// });
